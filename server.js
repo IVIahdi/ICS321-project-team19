383ca7c5fc1e954954a0c277818302a8c11a396a
IVIahdi <mahdi.11231@gmail.com>
2022-12-10 14:50:19 +0300

    u

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const db = require(__dirname + '/db');
 console.log(db.query("select * from package"))
 
+
+
 const server = express();
 const port = 8000;
+server.set('view engine', 'ejs')
 
 server.get('/', (req,res)=>{
     res.sendFile(__dirname + '/main.html')
@@ -18,13 +21,14 @@ server.get('/admin', (req,res)=>{
 })
 
 server.get('/admin/reports', (req,res)=>{
-    var q = "show tables;"
+    var q = 'select * from package;'
     db.query(q, (e,d)=>{
         if (e){
             throw e
         }
         else {
-            res.sendFile(__dirname + '/reports.html')
+            // res.sendFile(__dirname + '/reports.html')
+            res.render('profile.ejs', {data: d, messages: 'A'})
         }
     })
 })
