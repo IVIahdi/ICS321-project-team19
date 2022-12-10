74a0f4706b4edab67c3cdc27d87a42b45cd20762
IVIahdi <mahdi.11231@gmail.com>
2022-12-10 12:12:53 +0300

    u

---
+++ db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: 'root',
+database: 'IVI'
+});
+
+connection.connect(function(err){
+    if (err){throw err;}
+    else{console.log("G");}
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,34 @@
+const express = require("express");
+const db = require(__dirname + '/db');
+console.log(db.query("select * from package"))
+
+const server = express();
+const port = 8000;
+
+server.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/main.html')
+})
+
+server.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/login.html')
+})
+
+server.get('/admin', (req,res)=>{
+    var q = "selcet * from package"
+    db.query(q, (e,d)=>{
+        if (e){
+            throw e
+        }
+        else {
+            res.render(q)
+        }
+    })
+    res.sendFile(__dirname + '/admin.html')
+})
 
 
-const http = require('http')
-http.createServer(function(req,res){
-    res.write('Hello world');
-    res.end();
-}).listen(8080);
+server.use(express.static("public"));
+server.listen(()=>{
+    server.listen(port, ()=>{
+        console.log(`http://localhost:${port}`);})
+    });
